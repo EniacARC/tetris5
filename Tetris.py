59fbd85e7d4a419ee2d96772b0c6d805e11740c7
U-LOZER <yonatan.chapal@gmail.com>
2024-05-08 14:29:19 +0300

    cleaned up the code a bit. reverted to udp only. right this time.

--- Tetris.py
+++ Tetris.py
@@ -1,5 +1,6 @@
 # Import your classes
 import pickle
+import random
 import struct
 import threading
 import time
@@ -11,7 +12,7 @@ from state import State
 
 from collections import OrderedDict
 
-# Constants
+# --- gui constants ---
 BLOCK_SIZE = 24
 MINI_BLOCK = 24 // 2
 WIDTH = 10  # Number of columns
@@ -21,14 +22,26 @@ WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
 BLACK = (0, 0, 0)
 
+# --- coms constants ---
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 7372
 BUFFER_SIZE = 2048
 
-MINI_BOARDS_POS = [(5, -4), (5, -57), (69, -4), (69, -57)]
-lock = threading.Lock()
-event_glob = threading.Event()
-key = ""
+LISTEN_IP = '0.0.0.0'
+LISTEN_PORT = random.randrange(2000, 2100)
+
+
+# define global vars
+# -- events ---
+change_event = threading.Event()
+# --- game global ---
+game_over = False  # set game to be true
+
+
+# MINI_BOARDS_POS = [(5, -4), (5, -57), (69, -4), (69, -57)]
+# lock = threading.Lock()
+# event_glob = threading.Event()
+# key = ""
 
 
 def draw_grid(screen, board, size, start_x, start_y):
@@ -64,8 +77,8 @@ def draw_next_piece(screen, piece):
         pygame.draw.rect(screen, piece.color, rect.inflate(-2, -2))
 
 
-def draw_other_player(screen, board, pos):
-    draw_grid(screen, board, MINI_BLOCK, MINI_BOARDS_POS[pos][0], MINI_BOARDS_POS[pos][1])
+# def draw_other_player(screen, board, pos):
+#     draw_grid(screen, board, MINI_BLOCK, MINI_BOARDS_POS[pos][0], MINI_BOARDS_POS[pos][1])
 
 
 def draw_board(screen, board):
@@ -77,100 +90,116 @@ def draw_board(screen, board):
     draw_grid(screen, board, BLOCK_SIZE, 16, -11)
 
 
+# def send_data(sock, data):
+#     b = pickle.dumps(data.board)
+#     # g = struct.pack('?', data.game_over)
+#     # to_send = b + b'|||' + g
+#     sock.sendto(b, (SERVER_IP, SERVER_PORT))
+#
+#
+# def update_player(players, id_code, board):
+#     with lock:
+#         players[id_code] = board
+
+
+# def recv_data(sock, players):
+#     # the game over is temporary and will be sent through tcp
+#     while not event_glob.is_set():
+#         try:
+#             data, addr = sock.recvfrom(BUFFER_SIZE)
+#             if data:
+#                 data = data.split(b'|||')  # id, board
+#                 update_player(players, data[0].decode(), pickle.loads(data[1]))
+#         except socket.error as err:
+#             pass
+
+# data = data.split(b'|||')
+# return pickle.loads(data[0]), struct.unpack('?', data[1])[0]
+
+
+# def establish_connection(sock):
+#     global key
+#     while key == '':
+#         try:
+#             sock.connect(("127.0.0.1", 12345))
+#             key_add = ''
+#             while True:
+#                 buff = sock.recv(1).decode()
+#                 if buff == '':
+#                     break
+#                 key_add += buff
+#             print(key_add)
+#             key = key_add
+#         except socket.error as err:
+#             key = ''
+
 def send_data(sock, data):
-    b = pickle.dumps(data.board)
-    # g = struct.pack('?', data.game_over)
-    # to_send = b + b'|||' + g
-    sock.sendto(b, (SERVER_IP, SERVER_PORT))
-
-
-def update_player(players, id_code, board):
-    with lock:
-        players[id_code] = board
-
-
-def recv_data(sock, players):
-    # the game over is temporary and will be sent through tcp
-    while not event_glob.is_set():
+    global game_over
+    while not game_over:
+        change_event.wait()
+        print("sending: ")
         try:
-            data, addr = sock.recvfrom(BUFFER_SIZE)
-            if data:
-                data = data.split(b'|||')  # id, board
-                update_player(players, data[0].decode(), pickle.loads(data[1]))
+            serialized_data = pickle.dumps(data)
+            sock.sendto(serialized_data, (SERVER_IP, SERVER_PORT))
         except socket.error as err:
-            pass
-            # data = data.split(b'|||')
-            # return pickle.loads(data[0]), struct.unpack('?', data[1])[0]
-
-
-def establish_connection(sock):
-    global key
-    while key == '':
-        try:
-            sock.connect(("127.0.0.1", 12345))
-            key_add = ''
-            while True:
-                buff = sock.recv(1).decode()
-                if buff == '':
-                    break
-                key_add += buff
-            print(key_add)
-            key = key_add
-        except socket.error as err:
-            key = ''
+            print(f"error! '{err}'")
+        finally:
+            change_event.clear()
 
 
 def main():
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    my_socket.bind(('0.0.0.0', 1234))
+    global game_over
+    # my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # my_socket.bind(('0.0.0.0', 1234))
 
-    tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #
+    # establish_connection(tcp_sock)
 
-    establish_connection(tcp_sock)
-
+    # --- define initial game states ---
+    # ----------------------------------
+    # ~ coms ~
+    # set udp socket
+    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_sock.bind(('0.0.0.0', LISTEN_PORT))
+    # ~ gui ~
+    # start pygame
     pygame.init()
     screen = pygame.display.set_mode(WINDOW_SIZE)
     pygame.display.set_caption("Tetris")
+
+    # set variables for time
     clock = pygame.time.Clock()
     drop_time = 0
     press_time = 0
+
+    # set start states for all the keys
     pressed_keys = {pygame.K_LEFT: False, pygame.K_RIGHT: False, pygame.K_UP: False}
 
+    # ~ game logic ~
     # Initialize board and state
     board = Board()
     state = State(board)
+
+    # create first piece
     state.generate_new_piece()
     state.board.place(state.x, state.y, state.current_piece)
-
-    temp_board = Board()
-
-    # create the players list
-    players = OrderedDict()  # made up from (id, board)
-
-    players_thread = threading.Thread(target=recv_data, args=(my_socket, players))
-    players_thread.start()
 
     # draw the screen
     screen.fill(WHITE)
     draw_board(screen, board)
     draw_next_piece(screen, state.next)
-
-    for i in range(4):
-        draw_other_player(screen, board, i)
-
     pygame.display.flip()
 
-    game_over = False
-
-    # send_interval = 1 / 60
+    # ~ threads ~
+    send_thread = threading.Thread(target=send_data, args=(udp_sock, state))
+    send_thread.start()
 
     while not game_over:
-        # every cycle send update
-        send_thread = threading.Thread(target=send_data, args=(my_socket, state))
-        send_thread.start()
-
-        # send_data(my_socket, state)
+        change = False  # if change happened, update the server
         dt = clock.tick(60)  # Cap the frame rate at 60 FPS
+
+        # add time passed
         drop_time += dt
         press_time += dt
         state.shift_x = 0  # each frame x is reset
@@ -180,19 +209,21 @@ def main():
                 game_over = True
             # Track key presses and releases
             elif event.type == pygame.KEYDOWN:
+                change = True
                 state.grace = state.grace_turns
                 if event.key == pygame.K_UP:
                     state.rotate()
                 else:
                     pressed_keys[event.key] = True
-                # pressed = True
 
             elif event.type == pygame.KEYUP:
                 pressed_keys[event.key] = False
 
+        # Handle continuous movement
         if press_time >= 50:
+            change = True
+
             press_time = 0
-            # Handle continuous movement
             if pressed_keys.get(pygame.K_LEFT):
                 state.shift_x -= 1
                 state.move_x()
@@ -202,37 +233,43 @@ def main():
             elif pressed_keys.get(pygame.K_DOWN):
                 state.move_y()
 
+        # it's time to drop the piece down
         if drop_time >= 100:
+            change = True
             drop_time = 0
             state.shift_x = 0
             state.move_y()
             game_over = state.game_over
 
+        # --- draw the new screen ---
+        # clear the screen
         screen.fill(WHITE)
+
+        # draw other boards:
+        # ~ temp ~
+        # ---
+        draw_grid(screen, board, MINI_BLOCK, 5, -4)
+        draw_grid(screen, board, MINI_BLOCK, 5, -57)
+
+        draw_grid(screen, board, MINI_BLOCK, 69, -4)
+        draw_grid(screen, board, MINI_BLOCK, 69, -57)
+        # ---
+
+        # draw the player and next piece
         draw_board(screen, board)
         draw_next_piece(screen, state.next)
 
-        with lock:
-            for index, (id_code, board_player) in enumerate(players.items()):
-                draw_other_player(screen, board_player, index)
-            for i in range(4 - len(players)):
-                draw_other_player(screen, temp_board, i)
-
+        # commit the new screen
         pygame.display.flip()
 
-        send_thread.join()
+        if change:
+            # send the new board
+            change_event.set()
+            # send_data(udp_sock, state.board)
 
+    send_thread.join()
     print("done")
-    event_glob.set()
-    send_data(my_socket, state)
-    players_thread.join()
     time.sleep(3)
-    # for row in reversed(state.board.board):
-    #     for element in row:
-    #         print(element, end=" ")
-    #     print()
-    # print()
-    tcp_sock.close()
     pygame.quit()
 
 
